5d06162047b0c179d45d9305108351e1fb6fbc20
JarredMartinez2 <jarmartinez@csumb.edu>
2024-05-04 22:17:22 -0700

    trying recursive onFinishCall hopefully it works (also taking a minibreak)

--- app/src/main/java/com/project2/triviaGame/play_page.java
+++ app/src/main/java/com/project2/triviaGame/play_page.java
@@ -24,6 +24,8 @@ public class play_page extends AppCompatActivity {
     private ProjectRepository repository;
     private TextView timerView, questionView;
     private Button correct, wrong1, wrong2, wrong3, start;
+    private int count = 0;
+    private List<Trivia> correctSet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,17 +38,17 @@ public class play_page extends AppCompatActivity {
         wrong2 = findViewById(R.id.incorrectAnswer2);
         wrong3 = findViewById(R.id.incorrectAnswer3);
         start = findViewById(R.id.startButton);
+        correctSet = new ArrayList<>();
 
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String name = getIntent().getStringExtra("nameOfSet");
-                List<Trivia> correctSet = new ArrayList<>();
+                List<Trivia> fullset = repository.getAllTriviaLogs();
                 if (name == null) {
                     name = "";
                 }
                 if (!name.isEmpty()) {
-                    List<Trivia> fullset = repository.getAllTriviaLogs();
                     for(int i = 0; i < fullset.size(); i++) {
                         if (fullset.get(i).getCategory().equals(name)) {
                             correctSet.add(fullset.get(i));
@@ -60,11 +62,12 @@ public class play_page extends AppCompatActivity {
                 wrong1.setVisibility(View.VISIBLE);
                 wrong2.setVisibility(View.VISIBLE);
                 wrong3.setVisibility(View.VISIBLE);
-                questionView.setText(correctSet.get(0).getQuestion());
-                correct.setText(correctSet.get(0).getCorrectAnswer());
-                wrong1.setText(correctSet.get(0).getWrongAnswer());
-                wrong2.setText(correctSet.get(0).getWrongAnswer2());
-                wrong3.setText(correctSet.get(0).getWrongAnswer3());
+
+                questionView.setText(correctSet.get(count).getQuestion());
+                correct.setText(correctSet.get(count).getCorrectAnswer());
+                wrong1.setText(correctSet.get(count).getWrongAnswer());
+                wrong2.setText(correctSet.get(count).getWrongAnswer2());
+                wrong3.setText(correctSet.get(count).getWrongAnswer3());
                 timer.start();
             }
         });
@@ -79,6 +82,7 @@ public class play_page extends AppCompatActivity {
         }
        @Override
        public void onFinish() {
+            count++;
            timerView.setText("");
            ViewCompat.setBackgroundTintList(wrong1, ContextCompat.getColorStateList(play_page.this, R.color.red));
            ViewCompat.setBackgroundTintList(wrong2, ContextCompat.getColorStateList(play_page.this, R.color.red));
@@ -89,10 +93,19 @@ public class play_page extends AppCompatActivity {
            } catch (InterruptedException e) {
                throw new RuntimeException(e);
            }
-           ViewCompat.setBackgroundTintList(wrong1, ContextCompat.getColorStateList(play_page.this, R.color.purple));
-           ViewCompat.setBackgroundTintList(wrong2, ContextCompat.getColorStateList(play_page.this, R.color.purple));
-           ViewCompat.setBackgroundTintList(wrong3, ContextCompat.getColorStateList(play_page.this, R.color.purple));
-           ViewCompat.setBackgroundTintList(correct, ContextCompat.getColorStateList(play_page.this, R.color.purple));
+           while (count < correctSet.size()) {
+               ViewCompat.setBackgroundTintList(wrong1, ContextCompat.getColorStateList(play_page.this, R.color.purple));
+               ViewCompat.setBackgroundTintList(wrong2, ContextCompat.getColorStateList(play_page.this, R.color.purple));
+               ViewCompat.setBackgroundTintList(wrong3, ContextCompat.getColorStateList(play_page.this, R.color.purple));
+               ViewCompat.setBackgroundTintList(correct, ContextCompat.getColorStateList(play_page.this, R.color.purple));
+               questionView.setText(correctSet.get(count).getQuestion());
+               correct.setText(correctSet.get(count).getCorrectAnswer());
+               wrong1.setText(correctSet.get(count).getWrongAnswer());
+               wrong2.setText(correctSet.get(count).getWrongAnswer2());
+               wrong3.setText(correctSet.get(count).getWrongAnswer3());
+               timer.start();
+           }
+
        }
        };
 
