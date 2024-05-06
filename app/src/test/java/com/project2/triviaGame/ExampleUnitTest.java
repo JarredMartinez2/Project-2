0e9f3f7628608ced7004de0ad75f56200d3afaa2
nathanvillafria <nvillafria@csumb.edu>
2024-05-05 19:09:48 -0700

    This includes changes made - new unit tests for the leaderboard.

--- app/src/test/java/com/project2/triviaGame/ExampleUnitTest.java
+++ app/src/test/java/com/project2/triviaGame/ExampleUnitTest.java
@@ -4,6 +4,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import com.project2.triviaGame.Database.entities.LeaderBoard;
+
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -13,5 +15,18 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    //Testing if the user value stored in the DB matches
+    @Test
+    public void getLbName() {
+        LeaderBoard leaderboard = new LeaderBoard(5,"newuser");
+        assertEquals("newuser", leaderboard.getLbName());
+    }
+    //Testing if the score value stored in the DB matches
+    @Test
+    public void getLbScore() {
+        LeaderBoard leaderboard = new LeaderBoard(5,"newuser");
+        assertEquals(5, leaderboard.getScore());
     }
 }
